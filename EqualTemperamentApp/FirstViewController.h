25a8e8947c18d3ebb98d327028c52d311b8eaac3
Bennett Lin <bennettslin@gmail.com>
2013-12-08 19:21:11 -0800

    Initial Commit

---
+++ EqualTemperamentApp/FirstViewController.h
@@ -0,0 +1,13 @@
+//
+//  FirstViewController.h
+//  EqualTemperamentApp
+//
+//  Created by Bennett Lin on 12/8/13.
+//  Copyright (c) 2013 Bennett Lin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FirstViewController : UIViewController
+
+@end
---
+++ EqualTemperamentApp/FirstViewController.m
@@ -0,0 +1,29 @@
+//
+//  FirstViewController.m
+//  EqualTemperamentApp
+//
+//  Created by Bennett Lin on 12/8/13.
+//  Copyright (c) 2013 Bennett Lin. All rights reserved.
+//
+
+#import "FirstViewController.h"
+
+@interface FirstViewController ()
+
+@end
+
+@implementation FirstViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
